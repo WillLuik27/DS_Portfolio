d237e30507a44e72ef06eaaf37eb7a70cf35ea08
William Luik <luikw25@mail.wlu.edu>
2024-10-22 22:23:24 -0400

    include navbar.js

---
+++ navbar.js
@@ -0,0 +1,23 @@
+// navbar.js
+// Since all files are in the root folder, no need for dynamic baseURL adjustment
+const baseURL = '';
+
+document.write(`
+<header>
+    <h1>Welcome to My Portfolio</h1>
+    <nav>
+        <ul>
+            <li><a href="${baseURL}index.html">Home</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropbtn">Projects</a>
+                <div class="dropdown-content">
+                    <a href="${baseURL}fire_page.html">Fire Model</a>
+                    <a href="${baseURL}scheduler_page.html">Scheduling Optimizer</a>
+                </div>
+            </li>
+            <li><a href="${baseURL}about.html">About Me</a></li>
+            <li><a href="${baseURL}resume.html">Resume</a></li>
+        </ul>
+    </nav>
+</header>
+`);
