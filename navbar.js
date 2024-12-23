295e1606075f19a69a3f47a106a29cf326cec33d
William Luik <luikw25@mail.wlu.edu>
2024-12-23 13:13:40 -0500

    including earth crust model

--- navbar.js
+++ navbar.js
@@ -13,6 +13,8 @@ document.write(`
                 <div class="dropdown-content">
                     <a href="${baseURL}fire_page.html">Fire Model</a>
                     <a href="${baseURL}scheduler_page.html">Scheduling Optimizer</a>
+                    <a href="${baseURL}thermal_eq.html">Earth Crust Model</a>
+
                 </div>
             </li>
             <li><a href="${baseURL}about.html">About Me</a></li>
